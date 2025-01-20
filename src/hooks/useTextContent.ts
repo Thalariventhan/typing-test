d2f1cc5aea5e693a7936faf63dec5ca075f56085
Thalari <thalari@hotmail.com>
2025-01-20 06:01:57 +0530

    feat: add custom hook useTextContent for managing text input and mistakes

---
+++ src/hooks/useTextContent.ts
@@ -0,0 +1,62 @@
+import { type Accessor, createSignal } from 'solid-js'
+import { generateRandomText } from '../commons/generateText'
+import useReport, { type ReportHook } from './useReport'
+
+export interface TextContent {
+	typedText: Accessor<string>
+	untypedText: Accessor<string>
+	mistake: Accessor<number>
+	updateMistake: () => void
+	updateText: (key: string) => void
+	removeText: () => void
+	generateText: () => void
+}
+
+const useTextContent = (): TextContent => {
+	const [textContent, setTextContent] = createSignal<string>('')
+	const [typedText, setTypedText] = createSignal<string>('')
+	const [untypedText, setUntypedText] = createSignal<string>('')
+	const [textIndex, setTextIndex] = createSignal<number>(0)
+	const { mistake, updateMistake }: ReportHook = useReport()
+
+	function generateText(): void {
+		const newText: string = generateRandomText()
+		setTextContent(newText)
+		setUntypedText(newText)
+	}
+
+	function updateText(key: string): void {
+		const __typedText: string = typedText()
+		const __textContent: string = textContent()
+		let __textIndex: number = textIndex()
+
+		if (key === __textContent.charAt(__textIndex)) {
+			__textIndex++
+			setTypedText(__typedText + key)
+			setTextIndex(__textIndex)
+			setUntypedText(textContent().slice(__textIndex))
+		} else {
+			updateMistake()
+		}
+	}
+
+	function removeText(): void {
+		let __textIndex: number = textIndex()
+		__textIndex--
+		setTypedText(typedText().slice(0, -1))
+		setTextIndex(__textIndex)
+		setUntypedText(textContent().slice(__textIndex))
+	}
+
+	return {
+		typedText,
+		untypedText,
+		mistake,
+		updateText,
+		removeText,
+		generateText,
+		updateMistake,
+	}
+}
+
+export default useTextContent
